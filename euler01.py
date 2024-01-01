e75e9dc61e572fce58f9f9d708f6334f7f545774
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-01 19:19:56 -0300

    adding type hints

--- euler01.py
+++ euler01.py
@@ -7,7 +7,7 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-def euler01(max):
+def euler01(max) -> int:
     return sum(i for i in range(max) if i % 3 == 0 or i % 5 == 0)
     
 
--- euler02.py
+++ euler02.py
@@ -8,7 +8,7 @@ By considering the terms in the Fibonacci sequence whose values do not
 exceed four million, find the sum of the even-valued terms.
 """
 
-def euler02(upper):
+def euler02(upper) -> int:
     total, past, current = 0, 1, 2
     while current < upper: 
         current, past = current + past, current
