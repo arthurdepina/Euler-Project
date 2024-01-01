e7107615f8354c3897a2ddfd64026aab4ba568e0
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-01 20:32:49 -0300

    Create euler03.py

---
+++ euler03.py
@@ -0,0 +1,28 @@
+"""
+The prime factors of 13195 are 5, 7, 14 and 29.
+
+What is the largest prime factor of the number 
+600851475143?
+"""
+
+from math import floor, sqrt
+
+def prime_numbers(limit):
+    primes = []
+    sieve = [True] * (limit + 1)
+    
+    for num in range(2, limit + 1):
+        if sieve[num]:
+            primes.append(num)
+            for i in range(num * num, limit + 1, num):
+                sieve[i] = False
+    
+    return primes
+
+
+def euler03(num) -> int:
+    for i in prime_numbers(floor(sqrt(num)))[::-1]:
+        if num % i == 0: return i
+
+
+print(euler03(600851475143))
