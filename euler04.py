e05cbc3ab6a95d2d7a2da65acbf8c40df04d9fb1
arthurdepina <84559827+arthurdepina@users.noreply.github.com>
2024-01-02 00:00:40 -0300

    Create euler04.py

---
+++ euler04.py
@@ -0,0 +1,20 @@
+"""
+Largest Palindrome Product
+
+A palindromic number reads the same both ways. The largest palindrome
+made from the product of two 2-digit numbers is 9009 = 91 * 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
+def isPalindrome(n) -> bool:
+    return str(n) == str(n)[::-1]
+
+def euler04() -> int:
+    palindrome = 0
+    for i in range (100, 1000):
+        for j in range(100, 1000):
+            if isPalindrome(i * j) and i * j > palindrome: palindrome = i * j
+    return palindrome
+
+print(euler04())
